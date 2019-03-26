d9496c8407c7457bba95ef5a272aaf666acb3b5d
Giovanni Mazzuoccolo <gio@mbp-di-giovanni.home>
2019-03-26 22:56:06 +0100

    Refactor car.js, using styled-components and installed sc

--- src/car/car.js
+++ src/car/car.js
@@ -1,15 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from 'styled-components';
 import "./car.css";
 
 export default function Car(props) {
   return (
-    <div className="car" draggable>
-      <img src={"/images/" + props.image} alt={props.model} />
-      <p>{props.model}</p>
-    </div>
+    <StyledCar draggable>
+      <StyledCarImage src={"/images/" + props.image} alt={props.model} />
+      <StyledCarText>{props.model}</StyledCarText>
+    </StyledCar>
   );
 }
+
+const StyledCar = styled.div`
+  width: 18%;
+  position: absolute;
+  z-index: 1;
+  bottom: 4px;
+  left: 40%;
+  background: #fff;
+  box-shadow: 1px 1px 3px #000;
+  border-radius: 8px;
+  cursor: move;
+  padding: 0.5%;
+`;
+
+const StyledCarImage = styled.img`
+  width: 100%;
+  float: left;
+  padding-bottom: 0.4%;
+`;
+
+const StyledCarText = styled.p`
+  text-align: center;
+  width: 100%;
+`;
 
 Car.propTypes = {
   image: PropTypes.string.isRequired,
